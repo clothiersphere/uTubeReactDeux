d582bb92fe349fecc7cd94518a9a8865973ba8db
Spacemixup <xchrislee@gmail.com>
2016-06-06 18:21:46 -0700

    Search Bar / API Call implement

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyB_-yxPxNAssDHut65MBgyVifPmt1PcVAg';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = { videos: [] };	
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (data) {
+			this.setState({videos: data});
+		});
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (data) {
+			this.setState({videos: data});
+		});
+	}
+
+	render() {
+		return ( 
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
